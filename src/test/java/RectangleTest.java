795d1179083c97ab32f2fcff99ccea531370a92b
caoquyen1913 <caoquyen1913@gmail.com>
2021-12-16 21:44:12 +0700

    Create RectangleTest and Add Unit Test

---
+++ src/test/java/RectangleTest.java
@@ -0,0 +1,42 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import utils.Validator;
+
+public class RectangleTest {
+    private final String[][] newCanvasArr = {
+            {"-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-"},
+            {"|", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "|"},
+            {"|", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "|"},
+            {"|", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "|"},
+            {"|", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "|"},
+            {"-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-"}
+    };
+
+    Validator validator = new Validator();
+
+    @Test
+    void checkPointOverCanvas() {
+        int x1 = -1;
+        int y1 = 2;
+        int x2 = 6;
+        int y2 = 2;
+        Rectangle rectangle = new Rectangle(newCanvasArr, x1, y1, x2, y2);
+        Assertions.assertTrue(line.isPointOverCanvas(x1, y1));
+        Assertions.assertFalse(line.isPointOverCanvas(x2, y2));
+    }
+
+    @Test
+    void draw() {
+        String[][] expectedResult = {
+                {"-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-"},
+                {"|", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "x", "x", "x", "x", "x", " ", " ", "|"},
+                {"|", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "x", " ", " ", " ", "x", " ", " ", "|"},
+                {"|", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "x", "x", "x", "x", "x", " ", " ", "|"},
+                {"|", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "|"},
+                {"-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-"}
+        };
+        Rectangle rectangle = new Rectangle(newCanvasArr, 14, 1, 18, 3);
+        String[][] result = rectangle.draw();
+        validator.testEqualArray(expectedResult, result);
+    }
+}
