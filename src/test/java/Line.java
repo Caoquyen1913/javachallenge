e089c0d691de42571546ed578a3a0023f6e832ac
caoquyen1913 <caoquyen1913@gmail.com>
2021-12-16 21:36:11 +0700

    Create LineTest And Add Unit Test

---
+++ src/test/java/Line.java
@@ -0,0 +1,78 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import utils.Validator;
+
+import java.util.Arrays;
+
+public class Line {
+    private final String[][] newCanvasArr = {
+            {"-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-" },
+            {"|", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "|" },
+            {"|", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "|" },
+            {"|", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "|" },
+            {"|", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "|" },
+            {"-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-" }
+    };
+
+    Validator validator = new Validator();
+
+    @Test
+    void checkPointOverCanvas(){
+        Line line = new Line(newCanvasArr);
+        int x1 = -1;
+        int y1 = 2;
+        int x2 = 6;
+        int y2 = 2;
+        Assertions.assertTrue(line.isPointOverCanvas(x1, y1));
+        Assertions.assertFalse(line.isPointOverCanvas(x2, y2));
+    }
+
+    @Test
+    void checkHorizontalLine(){
+        int x1 = 1;
+        int y1 = 2;
+        int x2 = 6;
+        int y2 = 2;
+        Line line = new Line(newCanvasArr, x1, y1, x2, y2);
+        Assertions.assertTrue(line.isHorizontalLine(x1, y1, x2, y2));
+        x1 = 1;
+        y1 = 2;
+        x2 = 6;
+        y2 = 3;
+        Assertions.assertFalse(line.isHorizontalLine(x1, y1, x2, y2));
+    }
+
+    @Test
+    void checkVerticalLine(){
+
+        int x1 = 6;
+        int y1 = 2;
+        int x2 = 6;
+        int y2 = 4;
+        Line line = new Line(newCanvasArr, x1, y1, x2, y2);
+        Assertions.assertTrue(line.isVerticalLine(x1, y1, x2, y2));
+        x1 = 1;
+        y1 = 2;
+        x2 = 6;
+        y2 = 3;
+        Assertions.assertFalse(line.isVerticalLine(x1, y1, x2, y2));
+    }
+
+    @Test
+    void draw(){
+        String[][] expectedResult = {
+                {"-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-" },
+                {"|", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "|" },
+                {"|", "x", "x", "x", "x", "x", "x", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "|" },
+                {"|", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "|" },
+                {"|", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "|" },
+                {"-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-" }
+        };
+        Line line = new Line(newCanvasArr,1, 2,6,2);
+        String[][] result = line.draw();
+        validator.testEqualArray(expectedResult, result);
+        line = new Line(newCanvasArr,1,2,6,3);
+        result = line.draw();
+        validator.testEqualArray(newCanvasArr,result);
+    }
+}
