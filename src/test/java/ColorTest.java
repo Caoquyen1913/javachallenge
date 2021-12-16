381bc5fb61969059095c6b025a274031fc74c2c9
caoquyen1913 <caoquyen1913@gmail.com>
2021-12-16 21:55:09 +0700

    Create ColorTest and Add Unit Test

---
+++ src/test/java/ColorTest.java
@@ -0,0 +1,62 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import utils.Validator;
+
+public class ColorTest {
+    private final String[][] newCanvasArr = {
+            {"-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-"},
+            {"|", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "|"},
+            {"|", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "|"},
+            {"|", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "|"},
+            {"|", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "|"},
+            {"-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-"}
+    };
+
+    private final String[][] newCanvasArrHasLine = {
+            {"-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-"},
+            {"|", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "|"},
+            {"|", "x", "x", "x", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "|"},
+            {"|", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "|"},
+            {"|", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "|"},
+            {"-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-"}
+    };
+
+    Validator validator = new Validator();
+
+    @Test
+    void fillColor() {
+        String[][] expectedResult = {
+                {"-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-"},
+                {"|", "o", "o", "o", "o", "o", "o", "o", "o", "o", "o", "o", "o", "o", "o", "o", "o", "o", "o", "o", "o", "|"},
+                {"|", "o", "o", "o", "o", "o", "o", "o", "o", "o", "o", "o", "o", "o", "o", "o", "o", "o", "o", "o", "o", "|"},
+                {"|", "o", "o", "o", "o", "o", "o", "o", "o", "o", "o", "o", "o", "o", "o", "o", "o", "o", "o", "o", "o", "|"},
+                {"|", "o", "o", "o", "o", "o", "o", "o", "o", "o", "o", "o", "o", "o", "o", "o", "o", "o", "o", "o", "o", "|"},
+                {"-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-"}
+        };
+        Color color = new Color(newCanvasArr, 10, 3, "o");
+        String[][] result = color.fillColor();
+        validator.testEqualArray(expectedResult, result);
+    }
+
+    @Test
+    void checkPointOverCanvas() {
+        int x = -1;
+        int y = 2;
+        Color color = new Color(newCanvasArr, x, y);
+        Assertions.assertTrue(color.isPointOverCanvas(x, y));
+        x = 1;
+        y = 2;
+        Assertions.assertFalse(color.isPointOverCanvas(x, y));
+    }
+
+    @Test
+    void checkPointDuplicateWithLineOrRectangle() {
+        int x = 1;
+        int y = 2;
+        Color color = new Color(newCanvasArrHasLine, x, y);
+        Assertions.assertTrue(color.isPointDuplicate(x, y));
+        x = 1;
+        y = 2;
+        Assertions.assertFalse(color.isPointDuplicate(x, y));
+    }
+}
